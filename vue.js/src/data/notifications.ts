79583b1709a5323d9c5bd78e2266841fbae632f2
Inbar Azulay <inbar@developershouse.nl>
2021-06-30 11:12:16 +0200

    Add lorem ipsum

--- vue.js/src/data/notifications.ts
+++ vue.js/src/data/notifications.ts
@@ -1,9 +1,9 @@
 import { INotification } from "@/exoform/types/domainTypes";
 
 const notifications: Array<INotification> = [
-  { id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), text: "test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test", type: "info" },
-  { id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), text: "test2", type: "warning" },
-  { id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), text: "test1", type: "error" },
+  { id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elementum felis. Pellentesque gravida dui eu lacus malesuada, a convallis ante efficitur. Etiam imperdiet lacus ac ornare accumsan. Curabitur et dignissim lorem. Maecenas semper orci eget sapien finibus vestibulum.", type: "info" },
+  { id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), text: "Pellentesque pulvinar nec sapien ac dignissim.", type: "warning" },
+  { id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), text: "Maecenas quis augue sed libero pretium placerat ac et ipsum.", type: "error" },
 ];
 
 export default notifications;
--- vue.js/src/views/Home.vue
+++ vue.js/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <h1>This is the home page</h1>
     <div id="exoform"></div>
     <v-btn color="primary" @click="btnClicked">Click me</v-btn>
   </div>
